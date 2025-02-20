838872234d7c651319c0db33b64a4df0b6891243
이다은 <162148102+Amyleefiwfjj@users.noreply.github.com>
2025-02-20 22:24:52 +0900

    s

---
+++ src/pages/Library/LibraryRegister.jsx
@@ -0,0 +1,12 @@
+// import React, { useState, useRef } from 'react';
+
+const LibraryRegister = () => {
+
+    return (
+        <div className="min-h-screen bg-gray-100 flex justify-center py-3">
+            <h1>도서목록</h1>
+        </div>
+    );
+
+}
+export default LibraryRegister;
