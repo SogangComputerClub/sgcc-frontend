1aa30b992477a123e2170e1da52f0ad41011eea2
이다은 <162148102+Amyleefiwfjj@users.noreply.github.com>
2025-03-04 15:04:40 +0900

    fixed. footer changed

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,13 +4,12 @@ const Footer = () => {
     return (
         <footer className="bg-gray-800 text-white py-4">
             <div className="container mx-auto text-center">
-                <h1 className="text-lg font-bold">This is Footer</h1>
-                <p className="text-sm mt-2">© 2025 My Website. All Rights Reserved.</p>
+                <p className="text-sm mt-2">© 2025 SGCC website. All Rights Reserved.</p>
                 <a
-                    href="https://www.instagram.com/your_instagram_page"
+                    href="https://www.instagram.com/sgcc_sogang/"
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="text-blue-400 hover:underline"
+                    className="text-white hover:text-gray-400"
                 >
                     인스타그램 바로가기
                 </a>
